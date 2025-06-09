d660c97ce5d2ee66bdb1a661b1d99f7c67fb4708
Judy Liu <judyliu930913@hotmail.com>
2025-06-09 23:22:40 +0100

    Developing

--- js/loadProjects.js
+++ js/loadProjects.js
@@ -1,5 +1,5 @@
 const projects = [
-    "https://j299liu.github.io/JinLiu-Portfolio/projects/MARCH_on_a_new_datum.html",
+    "projects/MARCH_on_a_new_datum.html",
     // Add more project filenames here...
   ];
   
